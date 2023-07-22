e7ae19fe54dff4469ce89d96ec439c9c25dc2243
Zach Barnes <16zacharybarnes@gmail.com>
2023-07-22 13:35:54 -0500

    added fetch link and got lat and lon values

--- develop/script.js
+++ develop/script.js
@@ -10,18 +10,94 @@ var searchBar = $('#search-bar');
 var searchBtn = $('#search-btn');
 var cityQue = $('#recently-searched-city');
 var form = $('#form');
+var searchInputVal = $('#search-bar');
+var currentDay = $('#current-day');
 
 // API CALLBACK LINK
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 
-// Adjusts the search links
-function apiCall() {
+function handleFormSubmit(event) {
+    event.preventDefault();
 
+    var searchInputVal = $('#search-bar').val().trim();
+
+    if (!searchInputVal) {
+        console.error('You need a search input value!');
+        return;
+    }
+
+    apiSearch(searchInputVal);
 }
 
+// Adjusts the search links
+function apiSearch(searchInputVal) {
+
+    var latAndLonLink = `http://api.openweathermap.org/geo/1.0/direct?q=&limit=1&appid=60211b1fb1aaf71717286d520f6cfd4c`;
+    
+    if (searchInputVal) {
+        latAndLonLink = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInputVal}&limit=1&appid=60211b1fb1aaf71717286d520f6cfd4c`;
+    }
+    
+    fetch(latAndLonLink)
+        .then(function (response) {
+            if(!response.ok) {
+                return Promise.reject(Error('Response is bad!'));
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var latValue = data[0].lat;
+            var lonValue = data[0].lon;
+
+            weatherLink = `api.openweathermap.org/data/2.5/forecast?lat=${latValue}&lon=${lonValue}&appid=60211b1fb1aaf71717286d520f6cfd4c`;
+            console.log(weatherLink);
+
+            // weatherLink WORKS ABOVE, BUT NOT BELOW THIS LINE
+
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+        weatherSearchApi();
+}
+
+// function weatherSearchApi() {
+
+//     var weatherLink = `api.openweathermap.org/data/2.5/forecast?lat=&lon=&appid=60211b1fb1aaf71717286d520f6cfd4c`;
+
+//     var latValue = '33.7489924';
+//     var lonValue = '-84.3902644';
+
+//     if(weatherLink) {
+//         weatherLink = `api.openweathermap.org/data/2.5/forecast?lat=${latValue}&lon=${lonValue}&appid=60211b1fb1aaf71717286d520f6cfd4c`;
+//     }
+
+//     fetch(weatherLink)
+//         .then(function (response) {
+//             if(!response.ok) {
+//                 return Promise.reject(Error('Response is bad!'));
+//             }
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+
+
+// }
+
+// API KEY
+// 60211b1fb1aaf71717286d520f6cfd4c
+
 // On search, calls apiCall function
-searchBtn.on('click', apiCall);
+searchBtn.on('click', handleFormSubmit);
 
 function searchHistory() {
     
