448d8eda761aaeb72303170d6ddf99a5e8f90118
Zach Barnes <16zacharybarnes@gmail.com>
2023-07-25 12:54:14 -0500

    added icons and weather data to display

--- develop/script.js
+++ develop/script.js
@@ -13,6 +13,43 @@ var cityQueContainer = $('#recently-searched-container');
 var form = $('#form');
 var searchInputVal = $('#search-bar');
 var currentDay = $('#current-day');
+var icon = $('.icon');
+var currentTime = dayjs().format('M/D/YYYY');
+
+//dayOne
+var temperatureOne = $('.temp-1');
+var windOne = $('.wind-1');
+var humidityOne = $('.humidity-1');
+var dateOne = $('.date-1');
+var iconOne = $('.icon-1');
+
+//dayTwo
+var temperatureTwo = $('.temp-2');
+var windTwo = $('.wind-2');
+var humidityTwo = $('.humidity-2');
+var dateTwo = $('.date-2');
+var iconTwo = $('.icon-2');
+
+//dayThree
+var temperatureThree = $('.temp-3');
+var windThree = $('.wind-3');
+var humidityThree = $('.humidity-3');
+var dateThree = $('.date-3');
+var iconThree = $('.icon-3');
+
+//dayFour
+var temperatureFour = $('.temp-4');
+var windFour = $('.wind-4');
+var humidityFour = $('.humidity-4');
+var dateFour = $('.date-4');
+var iconFour = $('.icon-4');
+
+//dayFive
+var temperatureFive = $('.temp-5');
+var windFive = $('.wind-5');
+var humidityFive = $('.humidity-5');
+var dateFive = $('.date-5');
+var iconFive = $('.icon-5');
 
 // API CALLBACK LINK
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
@@ -20,9 +57,15 @@ var currentDay = $('#current-day');
 // On search, calls apiCall function
 searchBtn.on('click', handleFormSubmit);
 
-
 function handleFormSubmit(event) {
     event.preventDefault();
+
+    $('#wicon').removeClass('hide');
+    $('#wicon-1').removeClass('hide');
+    $('#wicon-2').removeClass('hide');
+    $('#wicon-3').removeClass('hide');
+    $('#wicon-4').removeClass('hide');
+    $('#wicon-5').removeClass('hide');
 
     var searchInputVal = $('#search-bar').val().trim();
 
@@ -34,6 +77,7 @@ function handleFormSubmit(event) {
     console.log(searchInputVal);
     setSearchHistory(searchInputVal);
     apiSearch(searchInputVal);
+    setUpDates(searchInputVal)
 }
 
 // ADD HTTPS TO LINK
@@ -76,7 +120,96 @@ function apiSearch(searchInputVal) {
                 .then(function (data) {
                     console.log(data);
 
-                    //code here
+                    //current day
+                    var windData = data.list[0].wind.gust;
+                    wind.text('Wind: ' + windData + ' MPH');
+
+                    var humidityData = data.list[0].main.humidity;
+                    humidity.text('Humidity: ' + humidityData + '%');
+
+                    var tempData = data.list[0].main.temp;
+                    temperature.text('Temp: ' + tempData + ' F');
+
+                    var iconData = data.list[0].weather[0].icon;
+                    icon.text(iconData);
+                    var iconUrl = 'http://openweathermap.org/img/w/' + iconData + '.png';
+                    $('#wicon').attr('src', iconUrl);
+
+                    //dayOne
+                    var windData = data.list[1].wind.gust;
+                    windOne.text('Wind: ' + windData + ' MPH');
+
+                    var humidityData = data.list[1].main.humidity;
+                    humidityOne.text('Humidity: ' + humidityData + '%');
+
+                    var tempData = data.list[1].main.temp;
+                    temperatureOne.text('Temp: ' + tempData + ' F');
+
+                    var iconData = data.list[1].weather[0].icon;
+                    iconOne.text(iconData);
+                    var iconUrl = 'http://openweathermap.org/img/w/' + iconData + '.png';
+                    $('#wicon-1').attr('src', iconUrl);
+                    console.log(iconUrl);
+
+                    //dayTwo
+                    var windData = data.list[2].wind.gust;
+                    windTwo.text('Wind: ' + windData + ' MPH');
+
+                    var humidityData = data.list[2].main.humidity;
+                    humidityTwo.text('Humidity: ' + humidityData + '%');
+
+                    var tempData = data.list[2].main.temp;
+                    temperatureTwo.text('Temp: ' + tempData + ' F');
+
+                    var iconData = data.list[2].weather[0].icon;
+                    iconTwo.text(iconData);
+                    var iconUrl = 'http://openweathermap.org/img/w/' + iconData + '.png';
+                    $('#wicon-2').attr('src', iconUrl);
+
+                    //dayThree
+                    var windData = data.list[3].wind.gust;
+                    windThree.text('Wind: ' + windData + ' MPH');
+
+                    var humidityData = data.list[3].main.humidity;
+                    humidityThree.text('Humidity: ' + humidityData + '%');
+
+                    var tempData = data.list[3].main.temp;
+                    temperatureThree.text('Temp: ' + tempData + ' F');
+
+                    var iconData = data.list[3].weather[0].icon;
+                    iconThree.text(iconData);
+                    var iconUrl = 'http://openweathermap.org/img/w/' + iconData + '.png';
+                    $('#wicon-3').attr('src', iconUrl);
+
+                    //dayFour
+                    var windData = data.list[4].wind.gust;
+                    windFour.text('Wind: ' + windData + ' MPH');
+
+                    var humidityData = data.list[4].main.humidity;
+                    humidityFour.text('Humidity: ' + humidityData + '%');
+
+                    var tempData = data.list[4].main.temp;
+                    temperatureFour.text('Temp: ' + tempData + ' F');
+
+                    var iconData = data.list[4].weather[0].icon;
+                    iconFour.text(iconData);
+                    var iconUrl = 'http://openweathermap.org/img/w/' + iconData + '.png';
+                    $('#wicon-4').attr('src', iconUrl);
+
+                    //dayFive
+                    var windData = data.list[5].wind.gust;
+                    windFive.text('Wind: ' + windData + ' MPH');
+
+                    var humidityData = data.list[5].main.humidity;
+                    humidityFive.text('Humidity: ' + humidityData + '%');
+
+                    var tempData = data.list[5].main.temp;
+                    temperatureFive.text('Temp: ' + tempData + ' F');
+
+                    var iconData = data.list[5].weather[0].icon;
+                    iconFive.text(iconData);
+                    var iconUrl = 'http://openweathermap.org/img/w/' + iconData + '.png';
+                    $('#wicon-5').attr('src', iconUrl);
                     
                 })
                 .catch(function (error) {
@@ -89,37 +222,27 @@ function apiSearch(searchInputVal) {
         .catch(function (error) {
             console.log(error);
         });
-
-        // weatherSearchApi(weatherLink);
 }
 
-// function weatherSearchApi(weatherLink) {
-
-//     var weatherLink = `api.openweathermap.org/data/2.5/forecast?lat=&lon=&appid=60211b1fb1aaf71717286d520f6cfd4c`;
-
-//     if(weatherLink) {
-//         weatherLink = `api.openweathermap.org/data/2.5/forecast?lat=${latValue}&lon=${lonValue}&appid=60211b1fb1aaf71717286d520f6cfd4c`;
-//     }
-
-//     fetch(weatherLink)
-//         .then(function (response) {
-//             if(!response.ok) {
-//                 return Promise.reject(Error('Response is bad!'));
-//             }
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         });
-
-
-// }
-
 function weatherDisplay(lastCity) {
-    // display all my api data to the window
+    //display local storage weather
+}
+
+function setUpDates(searchInputVal) {
+    var dateAndSearch = searchInputVal + ' ' + currentTime;
+
+    var dayOne = dayjs().day(3).format('M/D/YYYY');
+    var dayTwo = dayjs().day(4).format('M/D/YYYY');
+    var dayThree = dayjs().day(5).format('M/D/YYYY');
+    var dayFour = dayjs().day(6).format('M/D/YYYY');
+    var dayFive = dayjs().day(7).format('M/D/YYYY');
+    dateOne.text(dayOne);
+    dateTwo.text(dayTwo);
+    dateThree.text(dayThree);
+    dateFour.text(dayFour);
+    dateFive.text(dayFive);
+
+    citySearched.text(dateAndSearch);
 }
 
 function setSearchHistory(searchInputVal) {
@@ -139,18 +262,6 @@ function getSearchHistory() {
         return;
     }
     else {
-        cityQue.on('click', weatherDisplay(lastCity));
+        weatherDisplay(lastCity);
     }
 }
-
-
-
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
